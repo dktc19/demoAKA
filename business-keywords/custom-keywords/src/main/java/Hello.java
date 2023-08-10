54ea1cd6e92cf71474c6f833131a54d97fa1c33a
minhlh5 <minhlh5@fpt.com.vn>
2023-08-10 11:27:09 +0700

    installapp

--- business-keywords/custom-keywords/src/main/java/Hello.java
+++ business-keywords/custom-keywords/src/main/java/Hello.java
@@ -1,9 +1,23 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Base64;
+import org.apache.commons.codec.binary.Base64;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import javax.imageio.ImageIO;
+
+import com.fpt.ivs.at.core.keywords.mobilekeyword.ElementActionKeyword;
 import com.fpt.ivs.at.core.object.Locator;
 import com.fpt.ivs.at.core.object.UIObject;
 import com.fpt.ivs.at.core.utilities.MobileDriverUtilities;
@@ -13,11 +27,16 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.imagecomparison.FeatureDetector;
 import io.appium.java_client.imagecomparison.FeaturesMatchingResult;
+import io.appium.java_client.imagecomparison.MatchingFunction;
 import io.appium.java_client.imagecomparison.OccurrenceMatchingOptions;
 import io.appium.java_client.imagecomparison.OccurrenceMatchingResult;
+import javassist.compiler.ast.Keyword;
 import net.bytebuddy.asm.Advice.Return;
-
+import java.awt.image.BufferedImage;
+import java.awt.*;
+import org.apache.commons.io.FileUtils;
 public class Hello {
 
     public String test() {
@@ -68,26 +87,77 @@ public class Hello {
     // .withEnabledVisualization());
     // }
 
-    public void getElementWithImage(UIObject target) {
+    public void getElementWithImage(UIObject target) throws IOException {
         String jsonDataString = target.toString();
         JsonObject jsonObject = JsonParser.parseString(jsonDataString).getAsJsonObject();
         JsonArray jsonArray = jsonObject.getAsJsonArray("locatorList");
         for (JsonElement jsonElement : jsonArray) {
             if (jsonElement.getAsJsonObject().get("strategy").getAsString().equals("Image")) {
                 AppiumDriver driver = MobileDriverUtilities.getDriver();
-                String cleanBase64Data = jsonElement.getAsJsonObject().get("value").getAsString().replaceFirst("data:image/png;base64,", "");
-                System.out.println("cleanBase64Data>>>>>>>>>>>>>>>>>>" + cleanBase64Data);
-                byte[] partialImage = Base64.getDecoder().decode(cleanBase64Data);
-                byte[] screenshot = Base64.getDecoder().decode(MobileDriverUtilities.captureScreenshot(driver));
+                String cleanBase64Data = jsonElement.getAsJsonObject().get("value").getAsString().replaceFirst("data:image/png;base64,", " ");
 
-                System.out.println("screenshot >>>>>>>>>>>>>>>>>>" + screenshot);
+                byte[] partialImage = Base64.decodeBase64(cleanBase64Data);
+                byte[] screenshot = ((TakesScreenshot) MobileDriverUtilities.getDriver()).getScreenshotAs(OutputType.BYTES);
+       
+                // Create an temporary file
+                Path findwaldo = Files.createTempFile(null, ".png");
+                Path waldo = Files.createTempFile(null, ".png");
+                writeBytesToFileApache(waldo,partialImage);
+                writeBytesToFileApache(findwaldo,screenshot);
+
                 OccurrenceMatchingResult result = driver
-                        .findImageOccurrence(screenshot, partialImage, new OccurrenceMatchingOptions()
-                                .withEnabledVisualization());
-                System.out.println(result.getVisualization().length); 
-                System.out.println(result.getRect());
+                        .findImageOccurrence( new File(findwaldo.toString()),  new File(waldo.toString()), new OccurrenceMatchingOptions()
+                        .withEnabledVisualization());
+                // System.out.println(">>>>>>>>>>>>>");
+                // System.out.println(result.getVisualization().length); 
+                // System.out.println(">>>>>>>>>>>>>");
+
+                Path rt = Files.createTempFile(null, ".png");
+                System.out.println(">>>>>>>>>>>>>"+ rt );
+                byte[] data = Base64.decodeBase64(result.getVisualization());
+                writeBytesToFileApache(rt,data);
+                ElementActionKeyword test = new ElementActionKeyword();
+                test.tapWithCoordinates(result.getRect().getX() + (result.getRect().getWidth() /2), result.getRect().getY() + (result.getRect().getHeight() /2));
             }
         }
     }
 
+ // Apache Commons IO
+    public static void writeBytesToFileApache(Path fileOutput, byte[] bytes) throws IOException {
+        FileUtils.writeByteArrayToFile(new File(fileOutput.toString()), bytes);
+    }
+    
+// convert Image to BufferedImage
+    public static BufferedImage convertToBufferedImage(Image img) {
+
+        if (img instanceof BufferedImage) {
+            return (BufferedImage) img;
+        }
+
+        // Create a buffered image with transparency
+        BufferedImage bi = new BufferedImage(
+                img.getWidth(null), img.getHeight(null),
+                BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D graphics2D = bi.createGraphics();
+        graphics2D.drawImage(img, 0, 0, null);
+        graphics2D.dispose();
+
+        return bi;
+    }
+public void getColorInImages() throws IOException{
+    AppiumDriver driver = MobileDriverUtilities.getDriver();
+    byte[] screenshotBytes = ((TakesScreenshot) MobileDriverUtilities.getDriver()).getScreenshotAs(OutputType.BYTES);
+    BufferedImage image = ImageIO.read(new ByteArrayInputStream (screenshotBytes));
+    int height = image.getHeight(), width = image.getWidth();
+    // Getting pixel color by position x and y 
+    //int clr = image.getRGB(height/2, width/2);
+    int clr = image.getRGB(477, 387);
+    int red =   (clr & 0x00ff0000) >> 16;
+    int green = (clr & 0x0000ff00) >> 8;
+    int blue =   clr & 0x000000ff;
+    System.out.println("Red Color value = " + red);
+    System.out.println("Green Color value = " + green);
+    System.out.println("Blue Color value = " + blue);
+    }
 }
---
+++ business-keywords/custom-keywords/src/main/java/utils/installapp.java
@@ -0,0 +1,95 @@
+package utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.openqa.selenium.MutableCapabilities;
+
+import com.fpt.ivs.at.core.utilities.MobileDriverUtilities;
+
+import io.appium.java_client.AppiumDriver;
+
+import com.fpt.ivs.at.core.Constants;
+import com.fpt.ivs.at.core.driver.WebdriverSocketClient;
+public class installapp {
+    
+
+public void install_apk(String fileURL){
+    
+    String directoryPath = System.getProperty("project.path") +  File.separator + "business-keywords" +  File.separator + "custom-keywords" +  File.separator + "src" +  File.separator + "main" +  File.separator + "java" +  File.separator + "resources" + File.separator;
+    String saveFilePath = directoryPath + File.separator + "intallapp.apk";
+    Boolean hasAPK = false;
+    File directory = new File(directoryPath);
+    
+    if (!directory.exists() || !directory.isDirectory()) {
+        System.out.println("Invalid directory path");
+    }
+
+    File[] files = directory.listFiles();
+    if (files == null) {
+        System.out.println("Error accessing directory");
+    }
+
+    for (File file : files) {
+        if (file.isFile() && file.getName().endsWith(".apk")) {
+            hasAPK = true;
+        }
+    }
+    
+     if(hasAPK == false){
+        try {
+            downloadFileFromURL(fileURL, saveFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("File download failed.");
+        }
+    }
+
+
+    AppiumDriver driver = MobileDriverUtilities.getDriver();
+    String deviceID = driver.getCapabilities().getCapability("udid").toString();
+    System.out.println(">>>>>>>>>>>>>>>>> "+ deviceID);
+    
+        
+    String cmd = "adb -s "+ deviceID +" install "+ saveFilePath;
+    System.out.println(">>>>>>>>>>>>>>>>> "+ cmd);
+    runCmd(cmd);
+   
+}
+    
+    public static void downloadFileFromURL(String fileURL, String saveFilePath) throws IOException {
+        URL url = new URL(fileURL);
+        URLConnection connection;
+
+        if(System.getProperty("http.proxyHost").isEmpty()){
+            connection = url.openConnection();
+        }else{
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(System.getProperty("http.proxyHost"), Integer.parseInt(System.getProperty("http.proxyPort"))));
+            connection = url.openConnection(proxy);
+        }
+
+        InputStream inputStream = connection.getInputStream();
+
+        try (FileOutputStream outputStream = new FileOutputStream(saveFilePath)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+        }
+        System.out.println("File downloaded successfully.");
+    }
+
+    public static void runCmd(String inputString) {
+        try {
+            Runtime.getRuntime().exec(inputString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
